1f0e798b091680a5f977fa1cc6900db2add022f6
Bibhav Shrestha <bibhavshrestha60@gmail.com>
2024-03-08 22:54:09 +0545

    Initial setup

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from "react"
+import './App.css'
+
+function App() {
+  return (
+    <>
+      <h1 className="text-center text-4xl font-medium text-indigo-800 mt-6">Todo App</h1>
+    </>
+  )
+}
+
+export default App
